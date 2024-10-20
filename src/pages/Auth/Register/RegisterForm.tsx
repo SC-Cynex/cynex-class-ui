be0a850d9bb189af0c9a845f5c6d8468f2fe400f
Israel Schroeder Moreira <israelschroederm@gmail.com>
2024-10-20 17:27:38 -0300

    testee

--- src/pages/Auth/Register/RegisterForm.tsx
+++ src/pages/Auth/Register/RegisterForm.tsx
@@ -50,6 +50,7 @@ export default function RegisterForm() {
     const handleRegister = async (_values: RegisterValues) => {
         // Lógica para registrar o usuário
         // Teste
+        console.log(_values);
     };
 
     return (
